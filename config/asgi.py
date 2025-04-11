2a625119a4fdfcc6b887a9a5cf193b97938caa38
Rasul Imyarzhanov <Rassul.Imyarzhanov@raimbek.com>
2025-04-12 00:44:24 +0500

    rename directory

--- config/asgi.py
+++ config/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for shortUrl project.
+ASGI config for config project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -8,9 +8,14 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
 """
 
 import os
+import sys
+from pathlib import Path
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'shortUrl.settings')
+BASE_DIR = Path(__file__).resolve().parent.parent
+sys.path.append(str(BASE_DIR / "apps"))
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
 application = get_asgi_application()
